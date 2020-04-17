7b93a8426be6bbe08ebf16439f07876a4a56677f
fredericohorst <frederico.horst@gmail.com>
2020-04-16 22:51:25 -0300

    adicionando biblioteca de funcoes basicas de estatistica

---
+++ basic_statistics.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+
+# ECDF
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+
+    # x-data for the ECDF: x
+    x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, n+1) / n
+
+    return x, y
+
+# Bootstrap Resampling
+
+# bootstrap replicas
+def bootstrap_replicate_1d(data, func):
+     """Generate bootstrap replicate of 1D data."""
+     bs_sample = np.random.choice(data, len(data))
+     return func(bs_sample)
+
+# many bootstraps replicas
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data, func)
+
+    return bs_replicates
